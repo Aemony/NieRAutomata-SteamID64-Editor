d95485414e27600330845465c7bf36c1dd40047b
Idearum <idearum@outlook.com>
2018-02-12 17:48:33 +0100

    Initial commit.

---
+++ NieR_Automata_Editor/Form1.Designer.cs
@@ -0,0 +1,243 @@
+﻿using System;
+
+namespace NieR_Automata_Editor
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.buttonOpenFile = new System.Windows.Forms.Button();
+            this.textBoxWorkingFile = new System.Windows.Forms.TextBox();
+            this.textBoxSteamID64 = new System.Windows.Forms.TextBox();
+            this.labelSteamID64 = new System.Windows.Forms.Label();
+            this.groupBoxSteamID = new System.Windows.Forms.GroupBox();
+            this.textBoxSteamID64_New = new System.Windows.Forms.TextBox();
+            this.linkLabelSteamID64_New = new System.Windows.Forms.LinkLabel();
+            this.labelSteamID_New = new System.Windows.Forms.Label();
+            this.buttonSteamIDUpdate = new System.Windows.Forms.Button();
+            this.linkLabelSteamID64 = new System.Windows.Forms.LinkLabel();
+            this.textBoxSteamID3 = new System.Windows.Forms.TextBox();
+            this.labelSteamID3 = new System.Windows.Forms.Label();
+            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+            this.toolStripStatusLabel1 = new System.Windows.Forms.ToolStripStatusLabel();
+            this.groupBoxSteamID.SuspendLayout();
+            this.statusStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // buttonOpenFile
+            // 
+            this.buttonOpenFile.Location = new System.Drawing.Point(259, 19);
+            this.buttonOpenFile.Name = "buttonOpenFile";
+            this.buttonOpenFile.Size = new System.Drawing.Size(75, 23);
+            this.buttonOpenFile.TabIndex = 0;
+            this.buttonOpenFile.Text = "Open file...";
+            this.buttonOpenFile.UseVisualStyleBackColor = true;
+            this.buttonOpenFile.Click += new System.EventHandler(this.buttonOpenFile_Click);
+            // 
+            // textBoxWorkingFile
+            // 
+            this.textBoxWorkingFile.Location = new System.Drawing.Point(23, 21);
+            this.textBoxWorkingFile.Name = "textBoxWorkingFile";
+            this.textBoxWorkingFile.ReadOnly = true;
+            this.textBoxWorkingFile.Size = new System.Drawing.Size(230, 20);
+            this.textBoxWorkingFile.TabIndex = 2;
+            this.textBoxWorkingFile.Text = "No file loaded...";
+            // 
+            // textBoxSteamID64
+            // 
+            this.textBoxSteamID64.Location = new System.Drawing.Point(158, 55);
+            this.textBoxSteamID64.Name = "textBoxSteamID64";
+            this.textBoxSteamID64.ReadOnly = true;
+            this.textBoxSteamID64.Size = new System.Drawing.Size(147, 20);
+            this.textBoxSteamID64.TabIndex = 5;
+            // 
+            // labelSteamID64
+            // 
+            this.labelSteamID64.AutoSize = true;
+            this.labelSteamID64.Location = new System.Drawing.Point(6, 58);
+            this.labelSteamID64.Name = "labelSteamID64";
+            this.labelSteamID64.Size = new System.Drawing.Size(97, 13);
+            this.labelSteamID64.TabIndex = 6;
+            this.labelSteamID64.Text = "Current SteamID64";
+            // 
+            // groupBoxSteamID
+            // 
+            this.groupBoxSteamID.Controls.Add(this.textBoxSteamID64_New);
+            this.groupBoxSteamID.Controls.Add(this.linkLabelSteamID64_New);
+            this.groupBoxSteamID.Controls.Add(this.labelSteamID_New);
+            this.groupBoxSteamID.Controls.Add(this.buttonSteamIDUpdate);
+            this.groupBoxSteamID.Controls.Add(this.linkLabelSteamID64);
+            this.groupBoxSteamID.Controls.Add(this.textBoxSteamID3);
+            this.groupBoxSteamID.Controls.Add(this.labelSteamID3);
+            this.groupBoxSteamID.Controls.Add(this.textBoxSteamID64);
+            this.groupBoxSteamID.Controls.Add(this.labelSteamID64);
+            this.groupBoxSteamID.Location = new System.Drawing.Point(23, 62);
+            this.groupBoxSteamID.Name = "groupBoxSteamID";
+            this.groupBoxSteamID.Size = new System.Drawing.Size(311, 213);
+            this.groupBoxSteamID.TabIndex = 7;
+            this.groupBoxSteamID.TabStop = false;
+            this.groupBoxSteamID.Text = "SteamID";
+            // 
+            // textBoxSteamID64_New
+            // 
+            this.textBoxSteamID64_New.Location = new System.Drawing.Point(157, 133);
+            this.textBoxSteamID64_New.MaxLength = 20;
+            this.textBoxSteamID64_New.Name = "textBoxSteamID64_New";
+            this.textBoxSteamID64_New.Size = new System.Drawing.Size(147, 20);
+            this.textBoxSteamID64_New.TabIndex = 13;
+            this.textBoxSteamID64_New.TextChanged += new System.EventHandler(this.textBoxSteamID64_New_TextChanged);
+            // 
+            // linkLabelSteamID64_New
+            // 
+            this.linkLabelSteamID64_New.ActiveLinkColor = System.Drawing.SystemColors.HotTrack;
+            this.linkLabelSteamID64_New.AutoSize = true;
+            this.linkLabelSteamID64_New.Enabled = false;
+            this.linkLabelSteamID64_New.LinkColor = System.Drawing.SystemColors.HotTrack;
+            this.linkLabelSteamID64_New.Location = new System.Drawing.Point(181, 156);
+            this.linkLabelSteamID64_New.Name = "linkLabelSteamID64_New";
+            this.linkLabelSteamID64_New.Size = new System.Drawing.Size(123, 13);
+            this.linkLabelSteamID64_New.TabIndex = 12;
+            this.linkLabelSteamID64_New.TabStop = true;
+            this.linkLabelSteamID64_New.Text = "Steam Community Profile";
+            this.linkLabelSteamID64_New.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.linkLabelSteamID64_New_LinkClicked);
+            this.linkLabelSteamID64_New.MouseEnter += new System.EventHandler(this.linkLabelSteamID64_New_MouseEnter);
+            this.linkLabelSteamID64_New.MouseLeave += new System.EventHandler(this.linkLabelSteamID64_New_MouseLeave);
+            // 
+            // labelSteamID_New
+            // 
+            this.labelSteamID_New.AutoSize = true;
+            this.labelSteamID_New.Location = new System.Drawing.Point(6, 136);
+            this.labelSteamID_New.Name = "labelSteamID_New";
+            this.labelSteamID_New.Size = new System.Drawing.Size(85, 13);
+            this.labelSteamID_New.TabIndex = 11;
+            this.labelSteamID_New.Text = "New SteamID64";
+            // 
+            // buttonSteamIDUpdate
+            // 
+            this.buttonSteamIDUpdate.Enabled = false;
+            this.buttonSteamIDUpdate.Location = new System.Drawing.Point(230, 181);
+            this.buttonSteamIDUpdate.Name = "buttonSteamIDUpdate";
+            this.buttonSteamIDUpdate.Size = new System.Drawing.Size(75, 23);
+            this.buttonSteamIDUpdate.TabIndex = 10;
+            this.buttonSteamIDUpdate.Text = "Update";
+            this.buttonSteamIDUpdate.UseVisualStyleBackColor = true;
+            this.buttonSteamIDUpdate.Click += new System.EventHandler(this.buttonSteamIDUpdate_Click);
+            this.buttonSteamIDUpdate.MouseEnter += new System.EventHandler(this.buttonSteamIDUpdate_MouseEnter);
+            this.buttonSteamIDUpdate.MouseLeave += new System.EventHandler(this.buttonSteamIDUpdate_MouseLeave);
+            // 
+            // linkLabelSteamID64
+            // 
+            this.linkLabelSteamID64.ActiveLinkColor = System.Drawing.SystemColors.HotTrack;
+            this.linkLabelSteamID64.AutoSize = true;
+            this.linkLabelSteamID64.Enabled = false;
+            this.linkLabelSteamID64.LinkColor = System.Drawing.SystemColors.HotTrack;
+            this.linkLabelSteamID64.Location = new System.Drawing.Point(183, 78);
+            this.linkLabelSteamID64.Name = "linkLabelSteamID64";
+            this.linkLabelSteamID64.Size = new System.Drawing.Size(122, 13);
+            this.linkLabelSteamID64.TabIndex = 9;
+            this.linkLabelSteamID64.TabStop = true;
+            this.linkLabelSteamID64.Text = "Steam Community profile";
+            this.linkLabelSteamID64.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.linkLabelSteamID64_LinkClicked);
+            this.linkLabelSteamID64.MouseEnter += new System.EventHandler(this.linkLabelSteamID64_MouseEnter);
+            this.linkLabelSteamID64.MouseLeave += new System.EventHandler(this.linkLabelSteamID64_MouseLeave);
+            // 
+            // textBoxSteamID3
+            // 
+            this.textBoxSteamID3.Location = new System.Drawing.Point(158, 29);
+            this.textBoxSteamID3.Name = "textBoxSteamID3";
+            this.textBoxSteamID3.ReadOnly = true;
+            this.textBoxSteamID3.Size = new System.Drawing.Size(147, 20);
+            this.textBoxSteamID3.TabIndex = 7;
+            // 
+            // labelSteamID3
+            // 
+            this.labelSteamID3.AutoSize = true;
+            this.labelSteamID3.Location = new System.Drawing.Point(6, 32);
+            this.labelSteamID3.Name = "labelSteamID3";
+            this.labelSteamID3.Size = new System.Drawing.Size(91, 13);
+            this.labelSteamID3.TabIndex = 8;
+            this.labelSteamID3.Text = "Current SteamID3";
+            // 
+            // statusStrip1
+            // 
+            this.statusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripStatusLabel1});
+            this.statusStrip1.Location = new System.Drawing.Point(0, 288);
+            this.statusStrip1.Name = "statusStrip1";
+            this.statusStrip1.Size = new System.Drawing.Size(357, 22);
+            this.statusStrip1.SizingGrip = false;
+            this.statusStrip1.TabIndex = 8;
+            this.statusStrip1.Text = "statusStrip1";
+            // 
+            // toolStripStatusLabel1
+            // 
+            this.toolStripStatusLabel1.Name = "toolStripStatusLabel1";
+            this.toolStripStatusLabel1.Size = new System.Drawing.Size(120, 17);
+            this.toolStripStatusLabel1.Text = "Open a file to begin...";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(357, 310);
+            this.Controls.Add(this.statusStrip1);
+            this.Controls.Add(this.groupBoxSteamID);
+            this.Controls.Add(this.textBoxWorkingFile);
+            this.Controls.Add(this.buttonOpenFile);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "NieR: Automata - SteamID64 Editor";
+            this.groupBoxSteamID.ResumeLayout(false);
+            this.groupBoxSteamID.PerformLayout();
+            this.statusStrip1.ResumeLayout(false);
+            this.statusStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button buttonOpenFile;
+        private System.Windows.Forms.TextBox textBoxWorkingFile;
+        private System.Windows.Forms.TextBox textBoxSteamID64;
+        private System.Windows.Forms.Label labelSteamID64;
+        private System.Windows.Forms.GroupBox groupBoxSteamID;
+        private System.Windows.Forms.TextBox textBoxSteamID3;
+        private System.Windows.Forms.Label labelSteamID3;
+        private System.Windows.Forms.LinkLabel linkLabelSteamID64;
+        private System.Windows.Forms.Button buttonSteamIDUpdate;
+        private System.Windows.Forms.Label labelSteamID_New;
+        private System.Windows.Forms.LinkLabel linkLabelSteamID64_New;
+        private System.Windows.Forms.TextBox textBoxSteamID64_New;
+        private System.Windows.Forms.StatusStrip statusStrip1;
+        private System.Windows.Forms.ToolStripStatusLabel toolStripStatusLabel1;
+    }
+}
+
---
+++ NieR_Automata_Editor/Form1.cs
@@ -0,0 +1,218 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+using System.Text.RegularExpressions;
+using System.Windows.Forms;
+
+/* 
+ * 56 bytes = SystemData.dat
+ * 1 128 bytes = GameData.dat
+ * > 1 128 bytes = SlotData.dat
+ * 
+ */
+
+namespace NieR_Automata_Editor
+{
+    public partial class Form1 : Form
+    {
+        private string filePath = null;
+        private int fileOffset = 0;
+        private byte[] byteSteamID64 = new byte[8];
+        private UInt32 steamID3 = 0;
+        private UInt64 steamID64 = 0;
+        private string lastStatus = "";
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void buttonOpenFile_Click(object sender, EventArgs e)
+        {
+            ReadSteamID();
+        }
+
+        private void ReadSteamID()
+        {
+            Console.WriteLine("========= ReadSteamID =========");
+
+            OpenFileDialog dlg = new OpenFileDialog();
+            dlg.InitialDirectory = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments) + "\\NieR_Automata";
+            dlg.Filter = "Data files (*.dat)|*.dat";
+
+            if (dlg.ShowDialog() == DialogResult.OK)
+            {
+                filePath = dlg.FileName;
+
+                if (filePath.Contains("SlotData"))
+                {
+                    // SlotData files stores the SteamID64 in offset 04-11, GameData and SystemData files stores the SteamID64 in offset 00-07
+                    fileOffset = 4;
+                }
+                else
+                {
+                    fileOffset = 0;
+                }
+
+                File.Copy(filePath, filePath + DateTime.Now.ToString("_yyyy-MM-dd_HH-mm-ss.bak"));
+
+                try {
+                    using (FileStream stream = new FileStream(filePath, FileMode.Open, FileAccess.Read))
+                    {
+                        stream.Position = fileOffset;
+                        stream.Read(byteSteamID64, 0, 8);
+                    }
+
+                    Console.WriteLine("Read: " + BitConverter.ToString(byteSteamID64));
+
+                    // Convert to proper IDs.
+                    // SteamID64 is stored as Little-Endian in the files, but as Intel is little-endian as well no reversal is needed
+                    steamID3 = BitConverter.ToUInt32(byteSteamID64, 0);
+                    textBoxSteamID3.Text = steamID3.ToString();
+                    Console.WriteLine("SteamID3: " + steamID3.ToString());
+
+                    steamID64 = BitConverter.ToUInt64(byteSteamID64, 0);
+                    textBoxSteamID64.Text = steamID64.ToString();
+                    Console.WriteLine("SteamID64: " + steamID64.ToString());
+                    
+                    // Create the Steam Community link
+                    linkLabelSteamID64.Links.Clear();
+                    LinkLabel.Link linkSteamCommunityProfile = new LinkLabel.Link();
+                    linkSteamCommunityProfile.LinkData = "http://steamcommunity.com/profiles/" + steamID64.ToString();
+                    linkLabelSteamID64.Links.Add(linkSteamCommunityProfile);
+                    linkLabelSteamID64.Enabled = true;
+
+                    // Misc
+                    textBoxWorkingFile.Text = filePath;
+                    textBoxWorkingFile.SelectionStart = textBoxWorkingFile.TextLength;
+                    toolStripStatusLabel1.Text = "Read from file: " + BitConverter.ToString(byteSteamID64);
+
+                    // Check if a new ID is already written, and if so, enable the button
+                    if (String.IsNullOrWhiteSpace(textBoxSteamID64_New.Text) == false && String.IsNullOrWhiteSpace(filePath) == false && textBoxSteamID64_New.Text != steamID64.ToString())
+                    {
+                        buttonSteamIDUpdate.Enabled = true;
+                    }
+                    else
+                    {
+                        buttonSteamIDUpdate.Enabled = false;
+                    }
+                } catch (Exception ex)
+                {
+                    throw ex;
+                }
+            }
+
+            Console.WriteLine("========= END ReadSteamID =========");
+        }
+
+        private void WriteSteamID()
+        {
+            Console.WriteLine("========= WriteSteamID =========");
+
+            steamID64 = Convert.ToUInt64(textBoxSteamID64_New.Text);
+            byteSteamID64 = BitConverter.GetBytes(steamID64);
+            steamID3 = BitConverter.ToUInt32(byteSteamID64, 0); // Relies on byteSteamID64 having been updated. 
+
+            Console.WriteLine("New bytes: " + BitConverter.ToString(byteSteamID64));
+            Console.WriteLine("New SteamID3: " + steamID3.ToString());
+            Console.WriteLine("New SteamID64: " + steamID64.ToString());
+            
+            // SteamID64 is stored as Little-Endian in the files, but as Intel is little-endian as well no reversal is needed
+
+            try
+            {
+                using (Stream stream = File.Open(filePath, FileMode.Open, FileAccess.Write))
+                {
+                    stream.Position = fileOffset;
+                    stream.Write(byteSteamID64, 0, 8);
+                }
+
+                textBoxSteamID3.Text = steamID3.ToString();
+                textBoxSteamID64.Text = steamID64.ToString();
+                toolStripStatusLabel1.Text = "Wrote to file: " + BitConverter.ToString(byteSteamID64);
+                buttonSteamIDUpdate.Enabled = false;
+
+            } catch (Exception ex)
+            {
+                throw ex;
+            }
+            Console.WriteLine("========= END WriteSteamID =========");
+        }
+
+        private void linkLabelSteamID64_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            Process.Start(e.Link.LinkData as string);
+        }
+
+        private void buttonSteamIDUpdate_Click(object sender, EventArgs e)
+        {
+            WriteSteamID();
+        }
+
+        private void textBoxSteamID64_New_TextChanged(object sender, EventArgs e)
+        {
+            textBoxSteamID64_New.Text = Regex.Replace(textBoxSteamID64_New.Text, @"[^\d]", "");
+
+            if(textBoxSteamID64_New.Text.Length > 0)
+            {
+                // Create the new Steam Community link
+                linkLabelSteamID64_New.Links.Clear();
+                LinkLabel.Link linkSteamCommunityProfile_New = new LinkLabel.Link();
+                linkSteamCommunityProfile_New.LinkData = "http://steamcommunity.com/profiles/" + textBoxSteamID64_New.Text;
+                linkLabelSteamID64_New.Links.Add(linkSteamCommunityProfile_New);
+                linkLabelSteamID64_New.Enabled = true;
+            } else
+            {
+                linkLabelSteamID64_New.Links.Clear();
+                linkLabelSteamID64_New.Enabled = false;
+            }
+
+            // Check if a current file is loaded
+            if (String.IsNullOrWhiteSpace(textBoxSteamID64_New.Text) == false && String.IsNullOrWhiteSpace(filePath) == false && textBoxSteamID64_New.Text != steamID64.ToString())
+            {
+                buttonSteamIDUpdate.Enabled = true;
+            }
+            else
+            {
+                buttonSteamIDUpdate.Enabled = false;
+            }
+        }
+
+        private void linkLabelSteamID64_MouseEnter(object sender, EventArgs e)
+        {
+            lastStatus = toolStripStatusLabel1.Text;
+            toolStripStatusLabel1.Text = linkLabelSteamID64.Links[0].LinkData as string;
+        }
+
+        private void linkLabelSteamID64_MouseLeave(object sender, EventArgs e)
+        {
+            toolStripStatusLabel1.Text = lastStatus;
+        }
+
+        private void linkLabelSteamID64_New_MouseEnter(object sender, EventArgs e)
+        {
+            lastStatus = toolStripStatusLabel1.Text;
+            toolStripStatusLabel1.Text = linkLabelSteamID64_New.Links[0].LinkData as string;
+        }
+
+        private void linkLabelSteamID64_New_MouseLeave(object sender, EventArgs e)
+        {
+            toolStripStatusLabel1.Text = lastStatus;
+        }
+
+        private void linkLabelSteamID64_New_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            Process.Start(e.Link.LinkData as string);
+        }
+
+        private void buttonSteamIDUpdate_MouseEnter(object sender, EventArgs e)
+        {
+
+        }
+
+        private void buttonSteamIDUpdate_MouseLeave(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
